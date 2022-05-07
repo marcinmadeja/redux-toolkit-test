44d6a50053d9987172abdb4882f75f1b47c91668
Marcin Madeja <madeja.m.b@gmail.com>
2022-05-07 10:53:49 +0200

    Finish redux set up. Connect redux to counter component

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,3 +7,7 @@ export const store = configureStore({
     counter: counterReducer
   },
 });
+
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
